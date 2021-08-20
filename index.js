4685c15b5011fb50bbdbeef9d352a935148ecba7
Lizzietrust <ohidaelizabeth3@gmail.com>
2021-08-20 11:42:17 -0700

    my commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = function (fullname){
+    const outputName = `My name is ${fullname}`;
+    return outputName;
+}
+console.log(name("Ohida Elizabeth"));
+
+document.write("Ohida Elizabeth");
